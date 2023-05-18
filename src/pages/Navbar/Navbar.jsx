986df1202ff117e4556172859f094a72473dffe6
mdyasin111 <mdyasin7636@gmail.com>
2023-05-18 19:15:09 +0600

    nabvar and banner section added

--- src/pages/Navbar/Navbar.jsx
+++ src/pages/Navbar/Navbar.jsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+// import logo1 from '../../assets/logo-1.svg'
+import logo2 from '../../assets/logo-2.svg'
 
 const Navbar = () => {
     return (
@@ -10,39 +13,28 @@ const Navbar = () => {
       </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
         <li><a>Item 1</a></li>
-        <li tabIndex={0}>
-          <a className="justify-between">
-            Parent
-            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
-          </a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
         <li><a>Item 3</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <div>
+      <Link>
+      <img src={logo2}/>
+      </Link>
+    </div>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li tabIndex={0}>
-        <a>
-          Parent
-          <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul className="p-2">
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
-        </ul>
-      </li>
-      <li><a>Item 3</a></li>
+      <li><a>Home</a></li>
+      <li><a>All Toys</a></li>
+      <li><a>My Toys</a></li>
+      <li><a>Add A Toy</a></li>
+      <li><a>Blogs</a></li>
     </ul>
   </div>
   <div className="navbar-end">
-    <a className="btn">Get started</a>
+    <Link to="/login">
+    <button className='btn btn-primary'>Login</button>
+    </Link>
   </div>
 </div>
         </div>
